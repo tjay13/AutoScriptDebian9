ca7737e4a100726a6dbaf8dd391cc7e77ed97e58
tjay13 <harrison_mari@yahoo.com>
2022-09-30 17:51:43 +0200

    Add files via upload

---
+++ Tools/connection.php
@@ -0,0 +1,141 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+ini_set('display_errors', '1');
+//include('config.php');
+
+$DB_host = '185.61.137.174';
+$DB_user = 'vpnquest1_tsholovpn';
+$DB_pass = 'Pass1234Five@';
+$DB_name = 'vpnquest1_tsholovpn';
+
+$mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+
+function encrypt_key($paswd)
+	{
+	  $mykey=getEncryptKey();
+	  $encryptedPassword=encryptPaswd($paswd,$mykey);
+	  return $encryptedPassword;
+	}
+	 
+	// function to get the decrypted user password
+	function decrypt_key($paswd)
+	{
+	  $mykey=getEncryptKey();
+	  $decryptedPassword=decryptPaswd($paswd,$mykey);
+	  return $decryptedPassword;
+	}
+	 
+	function getEncryptKey()
+	{
+		$secret_key = md5('tsholovpn');
+		$secret_iv = md5('vpntsholo');
+		$keys = $secret_key . $secret_iv;
+		return encryptor('encrypt', $keys);
+	}
+	function encryptPaswd($string, $key)
+	{
+	  $result = '';
+	  for($i=0; $i<strlen ($string); $i++)
+	  {
+		$char = substr($string, $i, 1);
+		$keychar = substr($key, ($i % strlen($key))-1, 1);
+		$char = chr(ord($char)+ord($keychar));
+		$result.=$char;
+	  }
+		return base64_encode($result);
+	}
+	 
+	function decryptPaswd($string, $key)
+	{
+	  $result = '';
+	  $string = base64_decode($string);
+	  for($i=0; $i<strlen($string); $i++)
+	  {
+		$char = substr($string, $i, 1);
+		$keychar = substr($key, ($i % strlen($key))-1, 1);
+		$char = chr(ord($char)-ord($keychar));
+		$result.=$char;
+	  }
+	 
+		return $result;
+	}
+	
+	function encryptor($action, $string) {
+		$output = false;
+
+		$encrypt_method = "AES-256-CBC";
+		//pls set your unique hashing key
+		$secret_key = md5('tsholovpn.info');
+		$secret_iv = md5('info.tsholovpn');
+
+		// hash
+		$key = hash('sha256', $secret_key);
+		
+		// iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+		//do the encyption given text/string/number
+		if( $action == 'encrypt' ) {
+			$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+			$output = base64_encode($output);
+		}
+		else if( $action == 'decrypt' ){
+			//decrypt the given text/string/number
+			$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+		}
+
+		return $output;
+	}
+
+
+
+$data = '';
+$premium = "is_active=1 AND is_duration > 0";
+
+$query = $mysqli->query("SELECT * FROM user
+WHERE ".$premium." ORDER by id_user ASC");
+if($query->num_rows > 0)
+{
+	while($row = $query->fetch_assoc())
+	{
+		$data .= '';
+		$username = $row['username'];
+		$password = decrypt_key($row['password']);
+		$password = encryptor('decrypt',$password);		
+		$data .= '/usr/sbin/useradd -p $(openssl passwd -1 '.$password.') -M '.$username.';'.PHP_EOL;
+	}
+}
+$location = '/etc/sshlogin/active.sh';
+$fp = fopen($location, 'w');
+fwrite($fp, $data) or die("Unable to open file!");
+fclose($fp);
+
+#In-Active and Invalid Accounts
+$data2 = '';
+$premium_deactived = "is_duration <= 0";
+$is_activate = "is_active=0";
+$freeze = "is_freeze=0";
+$suspend = "is_suspend=0";
+
+$query2 = $mysqli->query("SELECT * FROM user 
+WHERE ".$suspend." OR ".$freeze." OR ".$premium_deactived ." OR ".$is_activate."
+");
+if($query2->num_rows > 0)
+{
+	while($row2 = $query2->fetch_assoc())
+	{
+		$data2 .= '';
+		$toadd = $row2['username'];	
+		$data2 .= '/usr/sbin/userdel '.$toadd.''.PHP_EOL;
+	}
+}
+$location2 = '/etc/sshlogin/inactive.sh';
+$fp = fopen($location2, 'w');
+fwrite($fp, $data2) or die("Unable to open file!");
+fclose($fp);
+
+$mysqli->close();
+?>
